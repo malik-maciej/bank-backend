556f63af8ffd1ab99d3563eb16fd8673a1df93dc
Maciej Malik <malik-maciej@o2.pl>
2020-07-14 14:00:04 +0200

    docket bean (swagger)

--- src/main/java/com/malik/bank/BankApplication.java
+++ src/main/java/com/malik/bank/BankApplication.java
@@ -2,11 +2,23 @@ package com.malik.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 public class BankApplication {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BankApplication.class, args);
